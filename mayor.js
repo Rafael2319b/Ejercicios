af0f167ea0dcc6beca5f19ba0c9801cbc3aae84e
Rafael2319b <kiznaiver1309@gmail.com>
2024-08-12 10:11:45 -0600

    Rafael

--- mayor.js
+++ mayor.js
@@ -1,4 +1,5 @@
 
+const { parse } = require('path');
 const readline = require('readline')
 
 const rl= readline.createInterface({
@@ -11,7 +12,18 @@ function maximoDeTres(n1,n2,n3){
 }
 
 rl.question('Ingrese el primer número:',(respuesta1)=>{
-    rl.question('Ingrese el número:',(respuesta2)=>{
-        
+    rl.question('Ingrese el segundo número:',(respuesta2)=>{
+        rl.question('Ingrese el tercer número:',(respuesta3)=>{
+            const num1 =parseFloat(respuesta1);
+            const num2 =parseFloat(respuesta2);
+            const num3 =parseFloat(respuesta3);
+
+            //Llamando a la función
+            const maximo = maximoDeTres(num1,num2,num3);
+            //Salida de la información
+            console.log(`El número mayor entre ${num1}, ${num2} y $(num3) es ${maximo}`)
+            rl.close();
+
+        })
     })
 })
